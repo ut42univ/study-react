291853b37efbb818d352a09f9d87f7f35720998f
tmba <ut42.nu@gmail.com>
2024-09-28 22:59:28 +0900

    不要なモジュールを削除

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,7 @@
 import Head from "next/head";
-import localFont from "next/font/local";
 import styles from "@/styles/Home.module.css";
 import { Footer } from "@/components/Footer";
 import { Main } from "@/components/Main";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function Home() {
   return (
