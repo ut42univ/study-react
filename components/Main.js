7c2e32ab228b31b91d3f6d7f1164e4069c888260
tmba <ut42.nu@gmail.com>
2024-09-28 23:33:28 +0900

    スタイルファイルの分割

--- components/Main.js
+++ components/Main.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "@/styles/Home.module.css";
+import styles from "./Main.module.css";
 import { Links } from "@/components/Links";
 import { Headline } from "@/components/Headline";
 
