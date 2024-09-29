3ebea6d8b80b5743e0fbe7ac9e55ae631d0cc73c
tmba <ut42.nu@gmail.com>
2024-09-29 21:00:31 +0900

    Refactor click event handler to increment count with useCallback

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,20 @@ import styles from "@/src/styles/Home.module.css";
 import { Footer } from "@/src/components/Footer";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  const handleClick = (event) => {
-    setCount((count) => count + 1);
-  };
+  const handleClick = useCallback(
+    (event) => {
+      if (count < 10) {
+        setCount((count) => count + 1);
+      }
+      console.log(count);
+    },
+    [count]
+  );
 
   return (
     <div>
