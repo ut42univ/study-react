768410bd171c8aa5ed187ec806c8851d0139f512
tmba <ut42.nu@gmail.com>
2024-09-29 18:02:02 +0900

    試しにクリックイベントを作成

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,17 @@ import styles from "@/src/styles/Home.module.css";
 import { Footer } from "@/src/components/Footer";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
+import { useCallback } from "react";
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((event) => {
+    console.log(event.target.href);
+    event.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <div>
       <Head>
@@ -12,6 +21,9 @@ export default function Home() {
       </Head>
       <div className={`${styles.page}`}>
         <Header />
+        <a href="/about" onClick={handleClick}>
+          ボタン
+        </a>
         <Main page="index" />
         <Footer />
       </div>
