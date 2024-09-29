083050cd8657946ea7974c212a713768cdaf45de
tmba <ut42.nu@gmail.com>
2024-09-29 18:53:02 +0900

    useStateを使用してカウンターを追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,15 @@ import styles from "@/src/styles/Home.module.css";
 import { Footer } from "@/src/components/Footer";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
+import { useState } from "react";
 
 export default function Home() {
+  const [count, setCount] = useState(1);
+
+  const handleClick = (event) => {
+    setCount((count) => count + 1);
+  };
+
   return (
     <div>
       <Head>
@@ -12,6 +19,8 @@ export default function Home() {
       </Head>
       <div className={`${styles.page}`}>
         <Header />
+        <h1>{count}</h1>
+        <button onClick={handleClick}>ボタン</button>
         <Main page="index" />
         <Footer />
       </div>
