4074680947e58973b7be55a458b24a1f6e07f36a
tmba <ut42.nu@gmail.com>
2024-09-29 23:11:10 +0900

    Refactor component props in _app.jsx, about.jsx, and index.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,18 @@
 import "@/src/styles/globals.css";
 import Head from "next/head";
+import { useCounter } from "@/src/hooks/useCounter";
+import { useInputArray } from "@/src/hooks/useInputArray";
 
 export default function App({ Component, pageProps }) {
+  const counter = useCounter();
+  const inputArray = useInputArray();
+
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <Component {...pageProps} {...counter} {...inputArray} />
     </>
   );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,18 @@ import styles from "@/src/styles/Home.module.css";
 import { Footer } from "@/src/components/Footer";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
-import { useCounter } from "@/src/hooks/useCounter";
-import { useInputArray } from "@/src/hooks/useInputArray";
 
-export default function Home() {
-  const { count, isShow, handleClick, handleDisplay } = useCounter();
-  const { text, array, handleChange, handleAdd } = useInputArray();
+export default function Home(props) {
+  const {
+    count,
+    isShow,
+    handleClick,
+    handleDisplay,
+    text,
+    array,
+    handleChange,
+    handleAdd,
+  } = props;
 
   return (
     <div>
